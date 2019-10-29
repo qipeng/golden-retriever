c806574a373a4ee86b7e754f169bb2a54d3ba15f
Peng Qi <qipeng@users.noreply.github.com>
2019-10-29 09:46:50 -0700

    Add download_corenlp.sh

---
+++ scripts/download_corenlp.sh
@@ -0,0 +1,4 @@
+wget http://nlp.stanford.edu/software/stanford-corenlp-full-2018-10-05.zip
+unzip stanford-corenlp-full-2018-10-05.zip
+rm stanford-corenlp-full-2018-10-05.zip
+export CORENLP_HOME=`pwd`/stanford-corenlp-full-2018-10-05
